18caa1a99b60fd770b78802177ddf941a472f9f0
Rafig <rafigmammadov@yahoo.com>
2024-05-11 22:24:58 +0400

    To uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    result = ""
+    for i in range(0, len(str)):
+        if 97 <= ord(str[i]) <= 122:
+            result += chr(ord(str[i]) - 32)
+        else:
+            result += str[i]
+    print(result + "\n", end='')
