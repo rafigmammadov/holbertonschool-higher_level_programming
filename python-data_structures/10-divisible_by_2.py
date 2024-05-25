2857d45af3dc21ed04f48df89b21c6c448538ed9
Rafig <rafigmammadov@yahoo.com>
2024-05-25 20:43:15 +0400

    Only by 2

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if my_list == []:
+        return None
+
+    result_list = []
+
+    for i in my_list:
+        if i % 2 == 0:
+            result_list.append(True)
+        else:
+            result_list.append(False)
+
+    return result_list
