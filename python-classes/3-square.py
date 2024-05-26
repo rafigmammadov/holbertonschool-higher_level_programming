d03f2c93cdd0c770d3f991ca64a0e7028e57da75
Rafig <rafigmammadov@yahoo.com>
2024-05-26 20:19:36 +0400

    Area of a square

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Documentation for Square class module
+"""
+
+
+class Square:
+    """
+    Square class that can calculate area
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
