7ca2fda72df1a7edb48dacf576d6cc2bd9facf95
Rafig <rafigmammadov@yahoo.com>
2024-05-26 20:57:34 +0400

    Printing a square

---
+++ python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Documentation for Square class module
+"""
+
+
+class Square:
+    """
+    Square class that can calculate area with print property
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
