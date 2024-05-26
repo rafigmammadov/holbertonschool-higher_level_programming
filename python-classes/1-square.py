eba8c956dfe7410803fb3bc64bf90a99bf728feb
Rafig <rafigmammadov@yahoo.com>
2024-05-26 20:12:38 +0400

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Documentation for Square class module
+"""
+
+
+class Square:
+    """
+    Square class that only has size attribute
+    """
+    def __init__(self, size):
+        self.__size = size
