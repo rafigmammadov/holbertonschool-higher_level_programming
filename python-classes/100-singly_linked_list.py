e5035fefbdfc3a887594f21803bff5b1ac99717c
Rafig <rafigmammadov@yahoo.com>
2024-05-26 22:40:45 +0400

    Singly linked list

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -29,7 +29,7 @@ class Node:
 
     @next_node.setter
     def next_node(self, value):
-        if value is not None or not isinstance(value, Node):
+        if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
