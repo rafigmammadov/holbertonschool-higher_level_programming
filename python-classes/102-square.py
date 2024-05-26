9d8d182c0ebe51d9283b8d2079840b89f492a9d8
Rafig <rafigmammadov@yahoo.com>
2024-05-26 23:00:35 +0400

    Compare 2 squares

---
+++ python-classes/102-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Documentation for Square class module
+"""
+
+
+class Square:
+    """
+    Square class that can calculate area with some getter and setter
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
