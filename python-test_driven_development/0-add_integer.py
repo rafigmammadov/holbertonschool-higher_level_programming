e3bfb3eed7f0a56134fbdecf946009d6bbb480a5
Rafig <rafigmammadov@yahoo.com>
2024-05-31 16:20:59 +0400

    Integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -9,9 +9,9 @@ def add_integer(a, b=98):
     Function that takes a and b as parameters and adds them
     """
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be integer")
+        raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
-        raise TypeError("b must be integer")
+        raise TypeError("b must be an integer")
 
     casted_a = int(a)
     casted_b = int(b)
