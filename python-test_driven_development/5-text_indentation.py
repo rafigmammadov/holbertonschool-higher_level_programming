65a93cbf7eedb9e3e3c3b277a61df8824c9e87c4
Rafig <rafigmammadov@yahoo.com>
2024-05-31 19:34:47 +0400

    Text indentation

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module that contains text_indentation() function
+"""
+
+
+def text_indentation(text):
+    """
+    Function that prints a text with 2 new lines
+    after each of these characters: ., ? and :
+    """
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    result = ""
+
+    for i in text:
+        result += i
+
+        if i in {'.', '?', ':'}:
+            result += '\n\n'
+
+    print(result, end="")
