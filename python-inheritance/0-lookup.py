10123dcc66a6b91adc628567247eb14db416b272
Rafig <rafigmammadov@yahoo.com>
2024-06-02 02:50:38 +0400

    Lookup

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module that contains lookup() function
+"""
+
+
+def lookup(obj):
+    """
+    Function that returns the list of
+    available attributes and methods of an object
+    """
+
+    return dir(obj)
