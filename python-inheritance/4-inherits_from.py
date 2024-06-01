c3d0ce88839772353daf77087faf5a557eeff75e
Rafig <rafigmammadov@yahoo.com>
2024-06-02 03:36:02 +0400

    Only sub class of

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module that contains inherits_from() function
+"""
+
+
+def inherits_from(obj, a_class):
+
+    """
+    Function that returns True if the object is
+    an instance of a class that inherited (directly or indirectly) from
+    the specified class ; otherwise False.
+    """
+
+    return not type(obj) is a_class and issubclass(obj, a_class)
