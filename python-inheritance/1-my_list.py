916ef307fa8059defab934d1962ae8e541850483
Rafig <rafigmammadov@yahoo.com>
2024-06-02 03:02:53 +0400

    My list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module that contains MyList class
+"""
+
+
+class MyList(list):
+    def print_sorted(self):
+        """
+        Function that prints the list, but sorted (ascending sort)
+        """
+        sorted_list = self[:]
+
+        for i in range(len(sorted_list)):
+            for k in range(len(sorted_list) - 1 - i):
+                if sorted_list[k] > sorted_list[k + 1]:
+                    sorted_list[k] = sorted_list[k + 1]
+                    sorted_list[k + 1] = sorted_list[k]
+
+        print(sorted_list)
