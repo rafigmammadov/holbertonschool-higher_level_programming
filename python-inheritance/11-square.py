abcc1767f5ea4f25ca9f029e581ab4cdcebf4bea
Rafig <rafigmammadov@yahoo.com>
2024-06-02 04:18:46 +0400

    Square #2

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module that contains class Square
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class Square that inherits from Rectangle class
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
