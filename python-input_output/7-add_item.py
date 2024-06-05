11ea54bcaf02c6476fb61028b9faad40bf9007aa
Rafig <rafigmammadov@yahoo.com>
2024-06-05 10:08:28 +0000

    Load, add, save

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module that can be defined as script
+"""
+import json
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+def main():
+    existing_data = load_from_json_file("add_item.json")
+
+    for i in range(1, len(sys.argv)):
+        existing_data.append(sys.argv[i])
+
+    save_to_json_file(existing_data, "add_item.json")
+
+    return load_from_json_file("add_item.json")
+
+
+if __name__ == "__main__":
+    main()
