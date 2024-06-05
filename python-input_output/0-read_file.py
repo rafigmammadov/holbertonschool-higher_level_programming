75d7999f51a01f68d4b77e198d0c7d5724fd0041
Rafig <rafigmammadov@yahoo.com>
2024-06-05 08:41:32 +0000

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,11 +3,10 @@
 Module that contains function read_file() function
 """
 
+
 def read_file(filename=""):
+    """
+    Function that reads a text file (UTF8) and prints it to stdout
+    """
     with open(filename, 'r', encoding='utf-8') as f:
         print(f.read())
-
-if __name__ == "__main__":
-    read_file = __import__('0-read_file').read_file
-
-    read_file("my_file_0.txt")
