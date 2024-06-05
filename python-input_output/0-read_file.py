e06f0c9d72d922bfcc43e969916b600f5831ab3f
Rafig <rafigmammadov@yahoo.com>
2024-06-05 08:47:18 +0000

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
     Function that reads a text file (UTF8) and prints it to stdout
     """
     with open(filename, 'r', encoding='utf-8') as f:
-        for line in f:
-            print(line)
+        file = f.read().splitlines()
+        print(file)
