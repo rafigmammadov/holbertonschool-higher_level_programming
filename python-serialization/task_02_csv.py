fe843e66c2a9ad77c19f708cdf5da430deca6692
Rafig <rafigmammadov@yahoo.com>
2024-06-12 13:44:35 +0400

    Converting CSV Data to JSON Format

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Module that contains convert_csv_to_json() function
+"""
+import csv
+import json
+
+
+def convert_csv_to_json(filename):
+    """
+    Function that takes the CSV filename as its
+    parameter and writes the JSON data to data.json
+    """
+    try:
+        data = {}
+
+        with open(filename, mode="r") as f:
+            csv_lines = csv.DictReader(f)
+
+            for rows in csv_lines:
+                key = rows['No']
+                data[key] = rows
+        with open("data.json", mode="w", encoding="utf-8") as f:
+            f.write(json.dumps(data, indent=4))
+
+    except FileNotFoundError:
+        print("file not found")
+        return False
+
+    except Exception as e:
+        print("The error has been occured")
+        return False
