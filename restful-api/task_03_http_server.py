5cb1c6d797c5ddfd009808abafab7b0a41e8e9f8
Rafig <rafigmammadov@yahoo.com>
2024-06-13 03:01:35 +0400

    Develop a simple API using Python with the  module

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+Module that contains some classes
+"""
+import http.server
+import json
+
+
+class HTTPServer(http.server.BaseHTTPRequestHandler):
+    """
+    Class that is subclass of http.server.BaseHTTPRequestHandler
+    """
+
+    def do_GET(self):
+
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content type', 'text/plain')
+            self.end_headers()
+            self.wfile.write('Hello, this is a simple API!'.encode())
+
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header('Content type', 'application/json')
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content type', 'application/json')
+            self.end_headers()
+            self.wfile.write('OK'.encode())
+
+        elif self.path == '/info':
+            self.send_response(200)
+            self.send_header('Content-Type', 'application/json')
+            self.end_headers()
+            data = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+                    }
+            self.wfile.write(json.dumps(data).encode())
+
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write('404 Not Found'.encode())
+
+
+def run(server_class=http.server.HTTPServer,
+        handler_class=HTTPServer, port=8000):
+
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd server on port {port}...')
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
