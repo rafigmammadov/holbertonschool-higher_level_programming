5dd1045ffac2c923b130ff6d3c132bb356ccfa37
Rafig <rafigmammadov@yahoo.com>
2024-06-27 00:38:07 +0400

    Go to UTF8

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -4,5 +4,5 @@ ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
 
 -- Convert the table to UTF-8
-ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
