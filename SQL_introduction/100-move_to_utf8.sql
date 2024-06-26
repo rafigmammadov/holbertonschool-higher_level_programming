dd4b5c7c37ad6e33505936c4c832176a4635107a
Rafig <rafigmammadov@yahoo.com>
2024-06-27 00:33:23 +0400

    Go to UTF8

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,6 @@
+-- Convert the database to UTF-8
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- Convert the table to UTF-8
+ALTER TABLE first_table CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
