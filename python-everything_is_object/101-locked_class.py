6cc9223f6bb87706365119447cfa35f6afae9f6c
Rafig <rafigmammadov@yahoo.com>
2024-08-14 14:00:16 +0400

    Low memory cost

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -7,7 +7,4 @@ class LockedClass:
     """
     a class LockedClass with no class or object attribute
     """
-    def __setattr__(self, key, value):
-        if key not in ('first_name', ):
-            raise AttributeError(f"'LockedClass' object has no attribute '{name}'")
-        super().__setattr__(key, value)
+    __slots__ = ['first_name']
