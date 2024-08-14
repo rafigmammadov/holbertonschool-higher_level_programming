49475df8329ecabca853d6944d514e488cec2ee8
Rafig <rafigmammadov@yahoo.com>
2024-08-14 13:58:41 +0400

    Low memory cost

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -4,6 +4,9 @@ Script that contains LockedClass class
 """
 
 class LockedClass:
+    """
+    a class LockedClass with no class or object attribute
+    """
     def __setattr__(self, key, value):
         if key not in ('first_name', ):
             raise AttributeError(f"'LockedClass' object has no attribute '{name}'")
